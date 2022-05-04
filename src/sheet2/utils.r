f6f5a7cb56485cd813a673e4dd74703692a4f549
Ferhat Topcu <topcu.ft@gmail.com>
2022-05-04 18:48:15 +0200

    removed csvtest from sheet2

--- src/sheet2/utils.r
+++ src/sheet2/utils.r
@@ -8,3 +8,20 @@ yellow <- function(str) paste(ANSI_RED, str, ANSI_RESET)
 cyan <- function(str) paste(ANSI_CYAN, str, ANSI_RESET)
 green <- function(str) paste(ANSI_GREEN, str, ANSI_RESET)
 red <- function(str) paste(ANSI_RED, str, ANSI_RESET)
+
+myread.test(
+  zile = "path/to/zile.tsv",
+  jikes =  "path/to/jikes.tsv",
+  junit = "path/to/junit.tsv",
+  junit20 = "path/to/zile.tsv",
+  smry = T # show data frame summary too
+)
+
+# and for developer:
+
+developer.test(
+  zile = myread.csvdata("path/to/zile.tsv"),
+  jikes = myread.csvdata("path/to/jikes.tsv"),
+  junit = myread.csvdata("path/to/junit.tsv"),
+  junit20 = myread.csvdata("path/to/junit20.tsv"),
+)
